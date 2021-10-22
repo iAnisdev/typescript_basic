eb40447401f3f45c4069fd466f59675282e6a62a
marahmanjs <imarahmandev@gmail.com>
2021-10-22 12:34:29 +0500

    types learning

---
+++ types/types.ts
@@ -0,0 +1,31 @@
+type bankAccountType = {
+    money: number,
+    deposit: (amount:number) => void, 
+    widraw: (amount:number) => void
+}
+
+var bankAccount:bankAccountType = {
+    money: 2000,
+    deposit: function(amount: number): void{
+        this.money +=  amount
+    },
+    widraw: function(amount: number) : void{
+        this.money -= amount
+    } 
+}
+
+var mySelf: {name: string , bankAccount: bankAccountType , hobbies: Array<string>} = {
+    name: "Anis",
+    bankAccount : bankAccount,
+    hobbies: ["Cricket" , "Coding"]
+}
+
+mySelf.bankAccount.deposit(3000)
+mySelf.bankAccount.deposit(1300)
+mySelf.bankAccount.deposit(2100)
+
+console.log(mySelf.bankAccount.money)
+
+mySelf.bankAccount.widraw(1900)
+
+console.log(mySelf.bankAccount.money)
